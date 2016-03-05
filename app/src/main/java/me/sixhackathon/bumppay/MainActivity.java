cbc623bb4d369dd89ff04ad5f32126696e85d99c
Robin B <robin.buergi@gmail.com>
2016-03-05 02:30:17 +0100

    added basic p2pkit functionality

--- app/src/main/java/me/sixhackathon/bumppay/MainActivity.java
+++ app/src/main/java/me/sixhackathon/bumppay/MainActivity.java
@@ -5,11 +5,36 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import ch.uepaa.p2pkit.P2PKitClient;
+import ch.uepaa.p2pkit.P2PKitStatusCallback;
+import ch.uepaa.p2pkit.StatusResult;
+import ch.uepaa.p2pkit.StatusResultHandling;
+import ch.uepaa.p2pkit.discovery.InfoTooLongException;
+import ch.uepaa.p2pkit.discovery.P2PListener;
+import ch.uepaa.p2pkit.discovery.entity.Peer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final P2PListener mP2PDiscoveryListener = new P2PListener() {
+        @Override
+        public void onP2PStateChanged(final int state) {}
+
+        @Override
+        public void onPeerDiscovered(final Peer peer) {
+            Log.i(MainActivity.class.toString(), "Peer found: " + peer.getDiscoveryInfo());
+        }
+
+        @Override
+        public void onPeerLost(final Peer peer) {}
+
+        @Override
+        public void onPeerUpdatedDiscoveryInfo(Peer peer) {}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +51,36 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // initialize p2pkit
+        final StatusResult result = P2PKitClient.isP2PServicesAvailable(this);
+        P2PKitStatusCallback mStatusCallback = new P2PKitStatusCallback() {
+            @Override
+            public void onEnabled() {
+                Log.i(MainActivity.class.toString(), "p2pkit enabled!");
+
+                P2PKitClient.getInstance(getApplicationContext()).getDiscoveryServices().addP2pListener(mP2PDiscoveryListener);
+
+                try {
+                    P2PKitClient.getInstance(getApplicationContext()).getDiscoveryServices().setP2pDiscoveryInfo("Hello p2pkit".getBytes());
+                } catch (InfoTooLongException e) {
+                    Log.i(MainActivity.class.toString(), "P2PListener | The discovery info is too long");
+                }
+            }
+
+            @Override
+            public void onSuspended() {}
+
+            @Override
+            public void onError(StatusResult result) {}
+        };
+
+        if (result.getStatusCode() == StatusResult.SUCCESS) {
+            P2PKitClient client = P2PKitClient.getInstance(this);
+            client.enableP2PKit(mStatusCallback, "eyJzaWduYXR1cmUiOiJpNSs1MHNZT1dTNVhlcFlOck9LNnJhNS9raGVneDVnMVZRK3owb2J4akRWUXMzL295T3ExWVJwa21VL0JDZWNnY21KM1JSbTlRU1hUY1hzMWVUdVJnbGp4blNLRGdocVMzd1pzWkQwVVIxbUZkbEJONjdiSFZFcHM5b3lkV2FRWXVkOTNxcmg4ZUpTdHI2SFJ1Rnk3TStMeHhlQTVlN0I4dE5jRkNzRlhnQjQ9IiwiYXBwSWQiOjE0NTksInZhbGlkVW50aWwiOjE2OTUxLCJhcHBVVVVJRCI6IkY2OEY0OEE0LUY5MDEtNDRERi05NDk5LTlCRjI0MEMzMkI5NiJ9");
+        } else {
+            StatusResultHandling.showAlertDialogForStatusError(this, result);
+        }
     }
 
     @Override
