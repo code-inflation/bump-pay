3fdb42deb7a317a888f1349f5084651bef68127c
disperate <julian.bigler@hotmail.com>
2016-03-05 12:12:19 +0100

    Improved payment UI

--- app/src/main/java/me/sixhackathon/bumppay/NewPayementActivity.java
+++ app/src/main/java/me/sixhackathon/bumppay/NewPayementActivity.java
@@ -1,7 +1,11 @@
 package me.sixhackathon.bumppay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewPayementActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class NewPayementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_payement);
+
+        Spinner dropdown = (Spinner)findViewById(R.id.b);
+        String[] items = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
+
+    /** Called when the user clicks the cancel button */
+    public void cancelGoHome(View view) {
+        //Maybe cleanup missing
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
